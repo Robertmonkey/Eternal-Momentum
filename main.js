f2d25d1d7b4e9d72cea97a894a221dd72af927b9
Robertmonkey <rbrobertmonkey@gmail.com>
2025-06-22 22:57:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // modules/main.js
-import { state, resetGame, loadPlayerState, savePlayerState } from './state.js';
-import { bossData } from './bosses.js';
-import { AudioManager } from './audio.js';
-import { updateUI, populateLevelSelect, showCustomConfirm } from './ui.js';
-import { gameTick, spawnBossesForStage, addStatusEffect, addEssence } from './gameLoop.js';
-import { usePower } from './powers.js';
-import * as utils from './utils.js';
-import { renderAscensionGrid, applyAllTalentEffects } from './ascension.js';
+import { state, resetGame, loadPlayerState, savePlayerState } from './modules/state.js';
+import { bossData } from './modules/bosses.js';
+import { AudioManager } from './modules/audio.js';
+import { updateUI, populateLevelSelect, showCustomConfirm } from './modules/ui.js';
+import { gameTick, spawnBossesForStage, addStatusEffect, addEssence } from './modules/gameLoop.js';
+import { usePower } from './modules/powers.js';
+import * as utils from './modules/utils.js';
+import { renderAscensionGrid, applyAllTalentEffects } from './modules/ascension.js';
 
 // --- DEBUG FUNCTION FOR TESTING ---
 // This function is now defined in the module's scope and explicitly attached to the window
@@ -213,7 +213,7 @@ window.addEventListener('load', (event) => {
         updateUI();
         loop();
     };
-
+    
     // --- FADE OUT LOADING SCREEN AND START ---
     setTimeout(() => {
         loadingScreen.style.opacity = '0';
