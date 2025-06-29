c82f6165a5930091ae39d43b2996e5f2972a5d64
Robertmonkey <rbrobertmonkey@gmail.com>
2025-06-28 21:47:44 -0400

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -53,3 +53,37 @@ export const SPAWN_WEIGHTS = {
     ricochetShot: 2, bulletNova: 2, repulsion: 1, black_hole: 1,
     gravity: 1, berserk: 1
 };
+
+// --- NEW: Master configuration for all stages ---
+export const STAGE_CONFIG = [
+    { stage: 1,  displayName: 'Splitter Sentinel',    bosses: ['splitter'] },
+    { stage: 2,  displayName: 'Reflector Warden',     bosses: ['reflector'] },
+    { stage: 3,  displayName: 'Vampire Veil',         bosses: ['vampire'] },
+    { stage: 4,  displayName: 'Gravity Tyrant',       bosses: ['gravity'] },
+    { stage: 5,  displayName: 'Swarm Link',           bosses: ['swarm'] },
+    { stage: 6,  displayName: 'Mirror Mirage',        bosses: ['mirror'] },
+    { stage: 7,  displayName: 'EMP Overload',         bosses: ['emp'] },
+    { stage: 8,  displayName: 'The Architect',        bosses: ['architect'] },
+    { stage: 9,  displayName: 'Aethel & Umbra',       bosses: ['aethel_and_umbra'] },
+    { stage: 10, displayName: 'Looping Eye',          bosses: ['looper'] },
+    { stage: 11, displayName: 'The Juggernaut',       bosses: ['juggernaut'] },
+    { stage: 12, displayName: 'The Puppeteer',        bosses: ['puppeteer'] },
+    { stage: 13, displayName: 'The Glitch',           bosses: ['glitch'] },
+    { stage: 14, displayName: 'Sentinel Pair',        bosses: ['sentinel_pair'] },
+    { stage: 15, displayName: 'The Basilisk',         bosses: ['basilisk'] },
+    { stage: 16, displayName: 'The Annihilator',      bosses: ['annihilator'] },
+    { stage: 17, displayName: 'The Parasite',         bosses: ['parasite'] },
+    { stage: 18, displayName: 'Quantum Shadow',       bosses: ['quantum_shadow'] },
+    { stage: 19, displayName: 'Time Eater',           bosses: ['time_eater'] },
+    { stage: 20, displayName: 'The Singularity',      bosses: ['singularity'] },
+    { stage: 21, displayName: 'The Miasma',           bosses: ['miasma'] },
+    { stage: 22, displayName: 'The Temporal Paradox', bosses: ['temporal_paradox'] },
+    { stage: 23, displayName: 'The Syphon',           bosses: ['syphon'] },
+    { stage: 24, displayName: 'The Centurion',        bosses: ['centurion'] },
+    { stage: 25, displayName: 'The Fractal Horror',   bosses: ['fractal_horror'] },
+    { stage: 26, displayName: 'The Obelisk',          bosses: ['obelisk'] },
+    { stage: 27, displayName: 'The Helix Weaver',     bosses: ['helix_weaver'] },
+    { stage: 28, displayName: 'The Epoch-Ender',      bosses: ['epoch_ender'] },
+    { stage: 29, displayName: 'The Shaper of Fate',   bosses: ['shaper_of_fate'] },
+    { stage: 30, displayName: 'The Pantheon',         bosses: ['pantheon'] }
+];
