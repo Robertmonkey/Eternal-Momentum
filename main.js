4a8789c065f40b7bee8651c9513848d7c65e2119
Robertmonkey <rbrobertmonkey@gmail.com>
2025-07-03 01:01:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 import { state, resetGame, loadPlayerState, savePlayerState } from './modules/state.js';
 import { bossData } from './modules/bosses.js';
 import { AudioManager } from './modules/audio.js';
-import { updateUI, populateLevelSelect, showCustomConfirm, populateOrreryMenu, showBossInfo } from './modules/ui.js';
+import { updateUI, populateLevelSelect, showCustomConfirm, populateOrreryMenu } from './modules/ui.js';
 import { gameTick, spawnBossesForStage, addStatusEffect, addEssence } from './modules/gameLoop.js';
 import { usePower } from './modules/powers.js';
 import * as utils from './modules/utils.js';
